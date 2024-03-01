5f975d32e00c9201f9cad6c8914cc1c268f99062
Anshul <anshulmhlvt@gmail.com>
2024-03-01 12:01:49 +0530

    minor bug fixes

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,6 +3,8 @@ const optionsContainer = document.querySelector('#options-container');
 const button = document.querySelector('button');
 const startButton = document.querySelector('.start');
 const stopButton = document.querySelector('.stop');
+const alertBox = document.querySelector('.alert-box');
+let selectedOption = null;
 
 const exampleBoard = [
     [".",".",".",".",".",".",".",".","."],
@@ -17,16 +19,28 @@ const exampleBoard = [
 ];
 
 const speed = 100;
-const delay = async () => {
-    await new Promise((done) => setTimeout(() => done(), speed));
+const delay = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), speed);
+    });
 }
-
-
 function buildBoard() {
+    for(let i = 0; i <= 9; i++) {
+        const num = document.createElement("div");
+        num.classList.add('options', 'flex', 'justify-center', 'align-center', 'font-wt-bold', 'br-radius');
+        if(i == 0) {
+            num.innerText = "C";
+        }
+        else {
+            num.innerText = i;
+        }
+        num.addEventListener('click', selectOption);
+        optionsContainer.append(num);
+    }
     for(let r = 0; r < 9; r++) {
         for(let c = 0; c < 9; c++) {
             const block = document.createElement('div');
-            block.classList.add('block');
+            block.classList.add('block', 'flex', 'justify-center', 'align-center', 'font-wt-bold');
             block.id = `${r}-${c}`;
             
             if(r == 2 || r == 5) {
@@ -35,24 +49,38 @@ function buildBoard() {
             if(c == 3 || c == 6) {
                 block.classList.add('border-left');
             }
-            if(exampleBoard[r][c] !== ".") {
-                block.innerText = exampleBoard[r][c];
-                block.classList.add('initial-block-color');
-            }
+            block.addEventListener('click', () => {
+                selectBlock(block, r, c);
+            });
             boardContainer.append(block);
         }
     }
-    
-    for(let i = 0; i <= 9; i++) {
-        const num = document.createElement("div");
-        num.classList.add('block');
-        num.classList.add('options');
-        num.innerText = i;
-        optionsContainer.append(num);
+}
+function selectOption() {
+    if(selectedOption) {
+        selectedOption.classList.remove('selected-color');
     }
+    selectedOption = this;
+    selectedOption.classList.add("selected-color");
 }
 
-
+function selectBlock(block, row, col) {
+    if(selectedOption) {
+        if(selectedOption.innerText === "C") {
+            if(block.innerText === "") {
+                return;
+            }
+            block.innerText = "";
+            block.classList.remove("selected-color");
+            exampleBoard[row][col] = ".";
+        }
+        else {
+            block.innerText = selectedOption.innerText;
+            block.classList.add("selected-color");
+            exampleBoard[row][col] = block.innerText;
+        }
+    }
+}
 const valid = async (row, col, c) => {
     for(let i = 0; i < 9; i++) {
         if(parseInt(exampleBoard[i][col]) === c) return false;
